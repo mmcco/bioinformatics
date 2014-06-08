43b7dfde68dabc954d7fbe1fa56483345ae7495c
Michael McConville <momcconville@gmail.com>
2014-06-07 20:26:05 -0400

    functioning version w/ "Unknown" category handing

--- repeats.py
+++ repeats.py
@@ -23,18 +23,6 @@ class Repeat:
         pass
 
 
-def parse_fasta(fns):
-    ret = defaultdict(bytearray)
-    for fn in fns:
-        with open(fn) as fh:
-            for line in fh:
-                if line[0] == '>':
-                    name = line[1:].rstrip()
-                else:
-                    ret[name].extend(line.rstrip())
-    return ret
-
-
 class Genome:
 
     def __init__(self, chromFilenames, repeatFilename, k, m):
@@ -46,7 +34,7 @@ class Genome:
         # contains a pointer to the tree root, populated by makeTree()
         self.tree = None
         # a dict of CategoryNodes indexed by name, populated by makeTree()
-        self.categoryNodes = None
+        self.catNodes = None
         self.chromosomes = defaultdict(bytearray)
         # length of each k-mer
         self.k = k
@@ -64,7 +52,7 @@ class Genome:
         for repeat in self.repeats[:10]:
             kmerGen = self.makeKmerGen(self.chromosomes[repeat.refid][repeat.ref_i-1:repeat.ref_f])
             repeat.minimizers = [self.minimize(kmer) for kmer in kmerGen]
-            print repeat.minimizers
+            #print repeat.minimizers
 
     # returns a k-mer generator
     def makeKmerGen(self, seq):
@@ -80,7 +68,7 @@ class Genome:
         def __init__(self, cat):
             self.name = cat[-1]
             self.parentName = "root" if len(cat) < 2 else cat[-2]
-            # makeTree eventually puts a pointer here
+            # makeTree eventually populates this
             self.parent = None
             # makeTree eventually populates this
             self.children = set()
@@ -89,30 +77,44 @@ class Genome:
     def makeTree(self):
         CategoryNode = self.CategoryNode    # for brevity
         self.tree = CategoryNode([None, "root"])
-        self.categoryNodes = {"root": self.tree}
-        # for every unique repeat category
-        categories = set((lambda r: r.ref_cl for repeat in self.repeats))
-        print type(self.repeats[0].ref_cl)
-        for cat in categories:
-            CategoryNode = CategoryNode(cat)
-            self.categoryNodes[cat.name] = cat
-            cat.parent.children.add(cat)
+        self.catNodes = {"root": self.tree}
+        otherID = 0    # a counter for handling the "Other" category below
+        # generator of all repeat categories
+        categories = (repeat.rep_cl for repeat in self.repeats)
+        for cats in categories:
+            # must add every heirarchy level here, because parent cats don't always have their own repeats
+            # (e.g. there being a repeat in cat LINE/Jockey doesn't imply that there's one in cat LINE)
+            branch = (cats[:i+1] for i in xrange(len(cats)))
+            for cat in branch:
+                # must remove duplicates manually because Python doesn't allow sets of lists
+                if cat[-1] in self.catNodes:
+                    continue
+                else:
+                    # an ugly conditional to ensure that unknowns are not grouped as a category,
+                    # but are rather children of root
+                    if cat[-1] == "Other":
+                        cat[-1] = "Unknown-" + str(otherID)
+                        otherID += 1
+                    catNode = CategoryNode(cat)
+                    self.catNodes[catNode.name] = catNode
+        # now loop again to populate parents and children
+        for catNode in self.catNodes.values():
+            if catNode.parentName:
+                catNode.parent = self.catNodes[catNode.parentName]
+                catNode.parent.children.add(catNode)
+            else:
+                catNode.parent = None
 
 
 def printTree(node, indent=0):
-    print ("\t" * indent), node.name
-    for child in root.children:
-        printTree(indent + 1, child)
+    print ("\t" * indent) + node.name
+    for child in node.children:
+        printTree(child, indent+1)
 
 
 if __name__ == "__main__":
-
-    #types = set(["/".join(repeat.rep_cl) for repeat in repeats])
-    #pprint(types)
 
     chromFilenames = ["chr2L.fa", "chr2LHet.fa", "chr2R.fa", "chr2RHet.fa", "chr3L.fa", "chr3LHet.fa", "chr3R.fa", "chr3RHet.fa", "chr4.fa", "chrM.fa", "chromFa.tar", "chrUextra.fa", "chrU.fa", "chrX.fa", "chrXHet.fa", "chrYHet.fa"]
     genome = Genome(chromFilenames, "dm3.fa.out", 31, 13)
     genome.makeTree()
     printTree(genome.tree)
-
-    repeatFile.close()
