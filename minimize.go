95a46b3a380b43b961790b74b5a4a3530f14e7ac
Michael McConville <momcconville@gmail.com>
2014-06-12 19:22:00 -0400

    Major updates, working match parser

--- minimize.go
+++ minimize.go
@@ -2,20 +2,135 @@
 
 package main
 
+
 import ("fmt"
         "log"
+        "os"
         "io/ioutil"
         "strings"
+        "strconv"
 )
 
-func main() {
+var err error
 
-    var err error
-    rawRepeatsBytes, err := ioutil.ReadFile("dm3/dm3.fa.out")
-    rawRepeats := string(rawRepeatsBytes)
+// uses 64-bit values, which is probably unnecessary for some fields
+// this is done for simplicity (and strconv compatibility)
+// it should probably be changed eventually
+type Match struct {
+    SW_Score int64
+    PercDiv float64
+    PercDel float64
+    PercIns float64
+    SeqName string
+    SeqStart int64
+    SeqEnd int64
+    Left int64
+    IsComplement bool
+    RepeatType string
+    RepeatClass string
+    HangingBases int64
+    RepeatStart int64
+    RepeatEnd int64
+    ID int64
+}
+
+
+// a minimal Match implementation for minimizing
+type MatchBare struct {
+    SeqName string
+    SeqStart int64
+    SeqEnd int64
+    IsComplement bool
+    RepeatType string
+    RepeatClass string
+    ID int64
+}
+
+
+type RefGenome struct {
+    Chroms []string
+}
+
+
+func checkError(err error) {
     if err != nil {
         log.Fatal(err)
     }
+}
+
+func parseMatches(matchLines []string) []Match {
+    var matches []Match
+    for i := 0; i < len(matchLines); i++ {
+        rawVals := strings.Fields(matchLines[i])
+        var match Match
+        match.SW_Score, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.PercDiv, err = strconv.ParseFloat(rawVals[1], 64)
+        checkError(err)
+        match.PercDel, err = strconv.ParseFloat(rawVals[2], 64)
+        checkError(err)
+        match.PercIns, err = strconv.ParseFloat(rawVals[3], 64)
+        checkError(err)
+        match.SeqName = rawVals[4]
+        match.SeqStart, err = strconv.ParseInt(rawVals[5], 10, 64)
+        checkError(err)
+        match.SeqEnd, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.Left, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.IsComplement = rawVals[8] == "C"
+        match.RepeatType = rawVals[9]
+        match.RepeatClass = rawVals[10]
+        match.HangingBases, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.RepeatStart, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.RepeatEnd, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.ID, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+
+        matches = append(matches, match)
+    }
+    return matches
+}
+
+
+func parseMatchBares(matchLines []string) []MatchBare {
+    var matches []MatchBare
+    for i := 0; i < len(matchLines); i++ {
+        rawVals := strings.Fields(matchLines[i])
+        var match MatchBare
+        match.SeqName = rawVals[4]
+        match.SeqStart, err = strconv.ParseInt(rawVals[5], 10, 64)
+        checkError(err)
+        match.SeqEnd, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+        match.IsComplement = rawVals[8] == "C"
+        match.RepeatType = rawVals[9]
+        match.RepeatClass = rawVals[10]
+        match.ID, err = strconv.ParseInt(rawVals[0], 10, 64)
+        checkError(err)
+
+        matches = append(matches, match)
+    }
+    return matches
+}
+
+
+func main() {
+
+    if len(os.Args) != 2 {
+        fmt.Println(len(os.Args), "args supplied")
+        fmt.Println("arg error - usage: ./minimize <reference genome dir>")
+        os.Exit(1)
+    }
+    genomeName := os.Args[1]
+    fmt.Println(genomeName)
+
+    rawRepeatsBytes, err := ioutil.ReadFile("dm3/dm3.fa.out")
+    rawRepeats := string(rawRepeatsBytes)
+    checkError(err)
 
     numLines := 0
     for i := 0; i < len(rawRepeats); i++ {
@@ -25,12 +140,12 @@ func main() {
     }
     fmt.Println("number of repeat file lines:", numLines)
 
-    repeats := strings.Split(rawRepeats, "\n")
-    // drop header
-    repeats = repeats[3:len(repeats)-1]
-    fmt.Println("number of parsed repeats:", len(repeats))
-    fmt.Println("first", repeats[0])
-    fmt.Println("second", repeats[1])
-    fmt.Println("third", repeats[2])
-    fmt.Println("last", repeats[len(repeats)-1])
+    matchLines := strings.Split(rawRepeats, "\n")
+    // drop header and empty end line
+    matchLines = matchLines[3:len(matchLines)-1]
+    fmt.Println("number of parsed matchLines:", len(matchLines))
+
+    matches := parseMatchBares(matchLines)
+
+    fmt.Println("number of parsed matches", len(matches))
 }
