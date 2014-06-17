742559b0f5d91be776150b4de1461d00efc9144e
Michael McConville <momcconville@gmail.com>
2014-06-16 20:22:27 -0400

    fix parseMatches(), add parentheses removal

--- minimize.go
+++ minimize.go
@@ -1,3 +1,5 @@
+// needs "Other" handling in tree formation
+
 /*
     A barebones (at the moment) Go script for parsing and minimizing repeats
 
@@ -20,6 +22,7 @@ import ("fmt"
         "io/ioutil"
         "strings"
         "strconv"
+        //"math"
 )
 
 var err error
@@ -35,14 +38,14 @@ type Match struct {
     SeqName string
     SeqStart int64
     SeqEnd int64
-    Left int64
-    IsComplement bool
-    RepeatType string
+    SeqRemains int64
+    IsRevComp bool
+    RepeatName string
     RepeatClass string
-    HangingBases int64
     RepeatStart int64
     RepeatEnd int64
-    ID int64
+    RepeatRemains int64
+    RepeatID int64
 }
 
 
@@ -51,10 +54,10 @@ type MatchBare struct {
     SeqName string
     SeqStart int64
     SeqEnd int64
-    IsComplement bool
-    RepeatType string
+    IsRevComp bool
+    RepeatName string
     RepeatClass string
-    ID int64
+    RepeatID int64
 }
 
 
@@ -62,6 +65,22 @@ type RefGenome struct {
     Name string
     // maps a chromosome name to a map of its sequences
     Chroms map[string](map[string]string)
+}
+
+
+type Kmer struct {
+    Kmer string
+    MinOffset int
+    IsRevComp bool
+    LCA *Repeat
+}
+
+
+type Repeat struct {
+    ID int64
+    Class []string
+    FullName string
+    parent *Repeat
 }
 
 
@@ -92,6 +111,18 @@ func parseMatches(matchLines []string) []Match {
     for i := 0; i < len(matchLines); i++ {
         rawVals := strings.Fields(matchLines[i])
         var match Match
+        match.IsRevComp = rawVals[8] == "C"
+
+        // remove enclosing parentheses
+        // !!! in the future, checkes to ensure that the parentheses exist should be added
+        // !!! it would also be sensible to check that rawVals[8] is either "C" or "+"
+        rawVals[7] = rawVals[7][1:len(rawVals[7])-1]
+        if match.IsRevComp {
+            rawVals[11] = rawVals[11][1:len(rawVals[11])-1]
+        } else {
+            rawVals[13] = rawVals[13][1:len(rawVals[13])-1]
+        }
+
         match.SW_Score, err = strconv.ParseInt(rawVals[0], 10, 64)
         checkError(err)
         match.PercDiv, err = strconv.ParseFloat(rawVals[1], 64)
@@ -100,24 +131,30 @@ func parseMatches(matchLines []string) []Match {
         checkError(err)
         match.PercIns, err = strconv.ParseFloat(rawVals[3], 64)
         checkError(err)
-        match.SeqName = rawVals[4]
+        match.SeqName = strings.TrimSpace(rawVals[4])
         match.SeqStart, err = strconv.ParseInt(rawVals[5], 10, 64)
         checkError(err)
-        match.SeqEnd, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.SeqEnd, err = strconv.ParseInt(rawVals[6], 10, 64)
         checkError(err)
-        match.Left, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.SeqRemains, err = strconv.ParseInt(rawVals[7], 10, 64)
         checkError(err)
-        match.IsComplement = rawVals[8] == "C"
-        match.RepeatType = rawVals[9]
-        match.RepeatClass = rawVals[10]
-        match.HangingBases, err = strconv.ParseInt(rawVals[0], 10, 64)
+        // match.IsComplement, rawVals[8], moved above
+        match.RepeatName = strings.TrimSpace(rawVals[9])
+        match.RepeatClass = strings.TrimSpace(rawVals[10])
+        match.RepeatStart, err = strconv.ParseInt(rawVals[11], 10, 64)
         checkError(err)
-        match.RepeatStart, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.RepeatEnd, err = strconv.ParseInt(rawVals[12], 10, 64)
         checkError(err)
-        match.RepeatEnd, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.RepeatRemains, err = strconv.ParseInt(rawVals[13], 10, 64)
         checkError(err)
-        match.ID, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.RepeatID, err = strconv.ParseInt(rawVals[14], 10, 64)
         checkError(err)
+
+        // necessary swaps to convert reverse complement repeat indexes to positive-strand indexes
+        if match.IsRevComp {
+            match.RepeatStart, match.RepeatEnd, match.RepeatRemains =
+                match.RepeatRemains, match.RepeatStart, match.RepeatRemains + (match.RepeatEnd - match.RepeatStart)
+        }
 
         matches = append(matches, match)
     }
@@ -130,15 +167,15 @@ func parseMatchBares(matchLines []string) []MatchBare {
     for i := 0; i < len(matchLines); i++ {
         rawVals := strings.Fields(matchLines[i])
         var match MatchBare
-        match.SeqName = rawVals[4]
+        match.SeqName = strings.TrimSpace(rawVals[4])
         match.SeqStart, err = strconv.ParseInt(rawVals[5], 10, 64)
         checkError(err)
-        match.SeqEnd, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.SeqEnd, err = strconv.ParseInt(rawVals[6], 10, 64)
         checkError(err)
-        match.IsComplement = rawVals[8] == "C"
-        match.RepeatType = rawVals[9]
-        match.RepeatClass = rawVals[10]
-        match.ID, err = strconv.ParseInt(rawVals[0], 10, 64)
+        match.IsRevComp = rawVals[8] == "C"
+        match.RepeatName = strings.TrimSpace(rawVals[9])
+        match.RepeatClass = strings.TrimSpace(rawVals[10])
+        match.RepeatID, err = strconv.ParseInt(rawVals[14], 10, 64)
         checkError(err)
 
         matches = append(matches, match)
@@ -204,12 +241,40 @@ func getMinimizer(kmer string, m int) int {
 
 
 func reverseComplement(seq string) string {
-    key := map[byte]byte{'a':'t', 't':'a', 'c':'g', 'g':'c', 'A':'T', 'T':'A', 'C':'G', 'G':'C'}
     var revCompSeq []byte
     for i := 0; i < len(seq); i++ {
-        revCompSeq = append(revCompSeq, key[seq[len(seq) - i - 1]])
+            switch seq[len(seq) - i - 1] {
+            case 'a': revCompSeq = append(revCompSeq, 't')
+            case 'A': revCompSeq = append(revCompSeq, 'T')
+            case 't': revCompSeq = append(revCompSeq, 'a')
+            case 'T': revCompSeq = append(revCompSeq, 'A')
+            case 'c': revCompSeq = append(revCompSeq, 'g')
+            case 'C': revCompSeq = append(revCompSeq, 'G')
+            case 'g': revCompSeq = append(revCompSeq, 'c')
+            case 'G': revCompSeq = append(revCompSeq, 'C')
+            }
     }
     return string(revCompSeq)
+}
+
+
+func max(a int64, b int64) int64 {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+
+func (repeat Repeat) print() {
+    for j := range repeat.Class {
+        for j_ := 0; j_ < j; j_++ {
+            fmt.Printf("\t")
+        }
+        fmt.Printf("%s\n", repeat.Class[j])
+    }
+    fmt.Println()
 }
 
 
@@ -223,44 +288,56 @@ func main() {
     genomeName := os.Args[1]
     refGenome := parseGenome(genomeName)
 
-    temp := strings.ToLower(refGenome.Chroms["chr2R"]["chr2R"])
-    for i := range temp {
-        ch := temp[i]
-        if ch != byte('a') && ch != byte('t') && ch != byte('g') && ch != byte('c') {
-            fmt.Printf("%d: %x\n", i, ch)
-        }
-    }
-    // shows N's
-    fmt.Println(temp[16668112:16668412])
-        
+    fmt.Println()
     for k, v := range refGenome.Chroms {
         for k_, v_ := range v {
             fmt.Printf("refGenome.Chroms[%s][%s] = %s . . . %s\n", k, k_, v_[:10], v_[len(v_)-10:])
             fmt.Printf("len(refGenome.Chroms[%s][%s]) = %d\n", k, k_, len(v_))
-            temp := strings.ToLower(v_)
-            cnt := 0
-            for i := range temp {
-                if temp[i] == byte('a') || temp[i] == byte('t') || temp[i] == byte('g') || temp[i] == byte('c') {
-                    cnt++
-                }
-            }
-            fmt.Println("number of bases:", cnt, "\n")
+        }
+        fmt.Println()
+    }
+
+    rawMatchesBytes, err := ioutil.ReadFile("dm3/dm3.fa.out")
+    checkError(err)
+    rawMatches := string(rawMatchesBytes)
+    numLines, matchLines := lines(rawMatches)
+    // drop header
+    fmt.Println("number of parsed matchLines (including header):", numLines)
+    matchLines = matchLines[3:]
+    fmt.Println("number of parsed matchLines (excluding header):", len(matchLines))
+    matches := parseMatches(matchLines)
+    fmt.Println("number of parsed matches:", len(matches))
+    fmt.Printf("\"%d\"\n", matches[0].RepeatID)
+
+    // we now populate a list of unique repeat types
+    // repeats are stored in the below slice, indexed by their ID
+    var repeats []Repeat
+    // we first determine the necessary size of the slice - we can't use append because matches are not sorted by repeatID
+    var repeatsSize int64 = 1
+    for i := range matches {
+        repeatsSize = max(repeatsSize, matches[i].RepeatID + 1)
+    }
+    repeats = make([]Repeat, repeatsSize)
+    // the real repeats begin at 1, we store root at 0
+    repeats[0] = Repeat{0, []string{"root"}, "root", nil}
+
+    // we now assign the actual repeats
+    for i := range matches {
+        id := matches[i].RepeatID
+        // don't bother overwriting
+        if id != repeats[id].ID {
+            repeats[id].ID = id
+            repeats[id].Class = append(strings.Split(matches[i].RepeatClass, "/"), matches[i].RepeatName)
+            repeats[id].FullName = strings.Join([]string{matches[i].RepeatClass, matches[i].RepeatName}, "/")
         }
     }
 
-    rawRepeatsBytes, err := ioutil.ReadFile("dm3/dm3.fa.out")
-    checkError(err)
-    rawRepeats := string(rawRepeatsBytes)
+    for i := 0; i < 15; i++ {
+        repeats[i].print()
+    }
 
-    numLines, matchLines := lines(rawRepeats)
-    fmt.Println("number of repeat file lines:", numLines)
-    // drop header
-    matchLines = matchLines[3:]
-    fmt.Println("number of parsed matchLines:", len(matchLines))
+    // matchBares := parseMatchBares(matchLines)
 
-    matches := parseMatchBares(matchLines)
-
-    fmt.Println("number of parsed matches", len(matches))
     fmt.Println("getMinimizer(\"ataggatcacgac\", 4) =", getMinimizer("ataggatcacgac", 4))
     fmt.Println("reverseComplement(\"aaAtGctACggT\") =", reverseComplement("aaAtGctACggT"))
 }
