3a446310a41bcd91aaed39e6a29e9bb5e0ae08de
Michael McConville <momcconville@gmail.com>
2014-07-09 21:14:29 -0400

    converting to proper package

---
+++ helper-rg.go
@@ -0,0 +1,114 @@
+package repeatgenome
+
+import (
+    "bytes"
+    "log"
+)
+
+var err error
+
+func checkError(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+// returns the number of lines and a slice of the lines
+func lines(byteSlice []byte) (numLines int, lines [][]byte) {
+    numLines = 0
+    for i := range byteSlice {
+        if byteSlice[i] == '\n' {
+            numLines++
+        }
+    }
+    lines = bytes.Split(byteSlice, []byte{'\n'})
+    // drop the trailing newlines
+    newline := []byte("\n")
+    for lastLine := lines[len(lines) - 1]; len(lines) > 0 && (len(lastLine) == 0 || bytes.Equal(lastLine, newline)); lastLine = lines[len(lines) - 1] {
+        lines = lines[:len(lines)-1]
+    }
+    return numLines, lines
+}
+
+func max(a int, b int) int {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func max32(a int32, b int32) int32 {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func maxU32(a uint32, b uint32) uint32 {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func max64(a int64, b int64) int64 {
+    if a > b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    } else {
+        return b
+    }
+}
+
+func minU64(a, b uint64) uint64 {
+    if a < b {
+        return a
+    } else {
+        return b
+    }
+}
+
+// needed for sort.Interface
+func (kmers Kmers) Len() int {
+    return len(kmers)
+}
+
+func (kmers Kmers) Swap(i, j int) {
+    kmers[i], kmers[j] = kmers[j], kmers[i]
+}
+
+func (kmers Kmers) Less(i, j int) bool {
+    return bytes.Compare(kmers[i].vals[:8], kmers[j].vals[:8]) == -1
+}
+
+// needed for sort.Interface
+type Uint64Slice []uint64
+
+func (uint64s Uint64Slice) Len() int {
+    return len(uint64s)
+}
+
+func (uint64s Uint64Slice) Swap(i, j int) {
+    uint64s[i], uint64s[j] = uint64s[j], uint64s[i]
+}
+
+func (uint64s Uint64Slice) Less(i, j int) bool {
+    return uint64s[i] < uint64s[j]
+}
+func boolToInt(a bool) int {
+    if a {
+        return 1
+    } else {
+        return 0
+    }
+}
---
+++ run-rg.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "jh/repeatgenome"
+    "os"
+    "runtime/pprof"
+)
+
+
+func main() {
+
+    if len(os.Args) < 2 {
+        fmt.Println("arg error - usage: ./minimize <flags> <reference genome dir>")
+        os.Exit(1)
+    }
+    genomeName := os.Args[len(os.Args) - 1]
+
+    cpuProfile := flag.Bool("cpuprof", false, "write cpu profile to file <genomeName>.cpuprof")
+    memProfile := flag.Bool("memprof", false, "write memory profile to <genomeName>.memprof")
+    debug := flag.Bool("debug", false, "run and print debugging tests")
+    dont_minimize := flag.Bool("nomin", false, "don't generate Kraken data structure")
+    writeJSON := flag.Bool("json", false, "write writeJSON representation of class tree to <genomeName>.classtree.json")
+    k_arg := flag.Uint("k", 31, "kmer length")
+    m_arg := flag.Uint("m", 15, "minimizer length")
+    flag.Parse()
+
+    if *cpuProfile {
+        fmt.Println("CPU profiler enabled")
+        fmt.Println()
+        f, err := os.Create(genomeName + ".cpuprof")
+        if err != nil {
+            panic(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
+
+    if *memProfile {
+        fmt.Println("memory profiler enabled")
+        fmt.Println()
+    }
+
+    if *debug {
+        fmt.Println("debug tests enabled")
+        fmt.Println()
+    }
+
+    var minimize bool
+    if *dont_minimize {
+        minimize = false
+        fmt.Println("Kraken data structure disabled")
+        fmt.Println()
+    } else {
+        minimize = true
+    }
+
+    if *writeJSON && minimize {
+        fmt.Println("class tree writeJSON write enabled")
+        fmt.Println()
+    } else if *writeJSON && !minimize {
+        fmt.Println("class tree writeJSON write enabled")
+        fmt.Println("WARNING: you are writing the class tree writeJSON without minimizing - all node sizes will be 0")
+        fmt.Println()
+    }
+
+    var k, m uint8
+    if *k_arg > 255 || *m_arg > 255 {
+        panic("k and m must be >= 255")
+    } else {
+        k = uint8(*k_arg)
+        m = uint8(*m_arg)
+        fmt.Println("k =", k)
+        fmt.Println("m =", m)
+        fmt.Println()
+    }
+
+    parseFlags := repeatgenome.ParseFlags{*debug, *cpuProfile, *memProfile, minimize, *writeJSON}
+    repeatGenome := repeatgenome.Generate(genomeName, k, m, parseFlags)
+    fmt.Println(repeatGenome.Name, "successfully generated - exiting")
+}
